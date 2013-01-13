38ab91a4446b157b6e43bb8a473e559b1407212e
JT Glaze <jimmyt0mmy@yahoo.com>
2013-01-13 16:02:46 -0500

    update csv monitor

--- src/Monitors/CsvMonitor.java
+++ src/Monitors/CsvMonitor.java
@@ -8,66 +8,66 @@ import java.lang.Exception;
 
 public class CsvMonitor implements Monitor {
 
-	private BufferedWriter out;
+    private BufferedWriter out;
 
-	public CsvMonitor(String filename) {
-		try {
-			out = new BufferedWriter(new FileWriter(filename), 32768);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+    public CsvMonitor(String filename) {
+        try {
+            out = new BufferedWriter(new FileWriter(filename), 32768);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		String s = "status, source address, destination address, ttl, payload, FailureCondition\n";
-		try {
-			out.write(s);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        String s = "status, source address, destination address, ttl, payload, FailureCondition\n";
+        try {
+            out.write(s);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void dropped(Packet packet, FailureCondition fc) {
-		String s = "";
-		s += "dropped, ";
-		s += packet.getSrcAddr() + ", ";
-		s += packet.getDstAddr() + ", ";
-		s += packet.getTtl() + ", ";
-		s += packet.getPayload() + ", ";
-		s += fc + "\n";
+    public void dropped(Packet packet, FailureCondition fc) {
+        String s = "";
+        s += "dropped, ";
+        s += packet.getSrcAddr() + ", ";
+        s += packet.getDstAddr() + ", ";
+        s += packet.getTtl() + ", ";
+        s += packet.getPayload() + ", ";
+        s += fc + "\n";
 
-		try {
-			out.write(s);
+        try {
+            out.write(s);
             out.flush();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void arrived(Packet packet) {
-		String s = "";
-		s += "arrived, ";
-		s += packet.getSrcAddr() + ", ";
-		s += packet.getDstAddr() + ", ";
-		s += packet.getTtl() + ", ";
-		s += packet.getPayload() + "\n";
-		
-		try  {
-			out.write(s);
+    public void arrived(Packet packet) {
+        String s = "";
+        s += "arrived, ";
+        s += packet.getSrcAddr() + ", ";
+        s += packet.getDstAddr() + ", ";
+        s += packet.getTtl() + ", ";
+        s += packet.getPayload() + "\n";
+
+        try  {
+            out.write(s);
             out.flush();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void close() {
-		try {
-			out.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void close() {
+        try {
+            out.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
